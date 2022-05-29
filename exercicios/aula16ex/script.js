307bb92a91e7c62e4d043538ee2a3c67456f5825
JCSkater <jcskater@gmail.com>
2022-05-29 00:44:28 -0300

    Criação do exerício 16: Analisador de números

--- exercicios/aula16ex/script.js
+++ exercicios/aula16ex/script.js
@@ -1,15 +1,46 @@
-const botaoAdicionar = document.querySelector("#botao1");
-const botaoFinalizar = document.querySelector("#botao2");
-const inputNumero = document.querySelector("#numero");
+let inputNumero = document.querySelector("#numero");
+let campoTexto = document.querySelector("#texto");
+let resultado = document.querySelector("#res");
+let valores = []
+
+let botaoAdicionar = document.querySelector("#botao1");
+let botaoFinalizar = document.querySelector("#botao2");
+
 
 botaoAdicionar.addEventListener("click", function() {
-    let campoTexto = document.querySelector("#texto");
-    campoTexto = document.createElement('option');
-    campoTexto.innerHTML = ` Adicionou o valor ${inputNumero.value} `;
-    document.querySelector("#texto").appendChild(campoTexto);
+    
+    if (inputNumero.value >= 1 && inputNumero.value <= 100) {
+        campoTexto = document.createElement('option');
+        campoTexto.innerHTML = `Valor ${inputNumero.value} adicionado`;
+        document.querySelector("#texto").appendChild(campoTexto);
+    }else {
+        if (inputNumero.value == "") {
+            alert('favor digitar um número');
+        } else if (inputNumero.value < 1) {
+            alert('Favor digitar um número entre 1 e 100');    
+        } else if (inputNumero.value > 100) {
+            alert('Favor digitar um número entre 1 e 100');
+        } else if (inputNumero.value == campoTexto.value) {
+            alert('digita um bagulho diferente');
+        }
+    }
 })
 
 botaoFinalizar.addEventListener("click", function() {
-    console.log('funcionou botao 2');
+    let paragrafo = document.createElement('p')
+    paragrafo.innerHTML = "Ao todo, temos X números cadastrados.";
+    resultado.appendChild(paragrafo);
+    paragrafo = document.createElement('p')
+    paragrafo.innerHTML = "O maior valor informado foi.";
+    resultado.appendChild(paragrafo);
+    paragrafo = document.createElement('p')
+    paragrafo.innerHTML = "O menor valor informado foi.";
+    resultado.appendChild(paragrafo);
+    paragrafo = document.createElement('p')
+    paragrafo.innerHTML = "Somando todos os valores temos.";
+    resultado.appendChild(paragrafo);
+    paragrafo = document.createElement('p')
+    paragrafo.innerHTML = "A média dos valores digitados é.";
+    resultado.appendChild(paragrafo);
 })
 
