bb57a5c6c078fa62434fecd458d751db90670cc8
JCSkater <jcskater@gmail.com>
2022-06-14 18:43:08 -0300

    atualização do exercício 16

--- exercicios/aula16ex/script.js
+++ exercicios/aula16ex/script.js
@@ -6,41 +6,37 @@ let valores = []
 let botaoAdicionar = document.querySelector("#botao1");
 let botaoFinalizar = document.querySelector("#botao2");
 
-
 botaoAdicionar.addEventListener("click", function() {
-    
     if (inputNumero.value >= 1 && inputNumero.value <= 100) {
+        valores.push(inputNumero.value);
         campoTexto = document.createElement('option');
         campoTexto.innerHTML = `Valor ${inputNumero.value} adicionado`;
         document.querySelector("#texto").appendChild(campoTexto);
     }else {
         if (inputNumero.value == "") {
             alert('favor digitar um número');
-        } else if (inputNumero.value < 1) {
+        } else if (inputNumero.value < 1 && inputNumero.value > 100) {
             alert('Favor digitar um número entre 1 e 100');    
-        } else if (inputNumero.value > 100) {
-            alert('Favor digitar um número entre 1 e 100');
-        } else if (inputNumero.value == campoTexto.value) {
-            alert('digita um bagulho diferente');
-        }
+        } 
     }
+    inputNumero.value = ''
+    inputNumero.focus()
+    resultado.innerHTML = ''
 })
 
 botaoFinalizar.addEventListener("click", function() {
-    let paragrafo = document.createElement('p')
-    paragrafo.innerHTML = "Ao todo, temos X números cadastrados.";
-    resultado.appendChild(paragrafo);
-    paragrafo = document.createElement('p')
-    paragrafo.innerHTML = "O maior valor informado foi.";
-    resultado.appendChild(paragrafo);
-    paragrafo = document.createElement('p')
-    paragrafo.innerHTML = "O menor valor informado foi.";
-    resultado.appendChild(paragrafo);
-    paragrafo = document.createElement('p')
-    paragrafo.innerHTML = "Somando todos os valores temos.";
-    resultado.appendChild(paragrafo);
-    paragrafo = document.createElement('p')
-    paragrafo.innerHTML = "A média dos valores digitados é.";
-    resultado.appendChild(paragrafo);
-})
+    if (campoTexto.value == '') {
+        alert('Adicione valores antes de finalizar');
+    } else {
+        let total = valores.length;
+        let maior;
 
+        let paragrafo = document.createElement('p')
+        paragrafo.innerHTML += `Ao todo, temos ${total} números cadastrados.`;
+        paragrafo.innerHTML += "<p>O maior valor informado foi.</p>";
+        paragrafo.innerHTML += "<p>O menor valor informado foi.</p>";
+        paragrafo.innerHTML += "<p>Somando todos os valores temos.</p>";
+        paragrafo.innerHTML += "<p>A média dos valores digitados é.</p>";
+        resultado.appendChild(paragrafo);
+    }
+});
